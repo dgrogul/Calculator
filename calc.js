0908cca9616dbf452ec02c663015b7f06c60a639
dgrogul <dgrogul28@gmail.com>
2023-09-20 10:57:59 -0700

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,26 @@
+// calculator.js
+
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function deleteLastChar() {
+    let currentValue = display.value;
+    if (currentValue.length > 0) {
+        display.value = currentValue.slice(0, -1);
+    }
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
